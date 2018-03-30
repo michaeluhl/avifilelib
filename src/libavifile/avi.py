e6594791629529ae6b061aaa760ad0474d87d4f3
Michael Uhl <michael.uhl@gmail.com>
2018-03-29 22:04:56 -0400

    Add documentation.

--- src/libavifile/avi.py
+++ src/libavifile/avi.py
@@ -1,3 +1,8 @@
+"""AVI File object
+
+This module contains the AviFile class.
+
+"""
 from contextlib import closing
 from struct import unpack
 
@@ -10,6 +15,36 @@ from libavifile.riff import RIFFChunk, ChunkTypeException
 
 
 class AviFileHeader(object):
+    """_`AVIMAINHEADER` structure
+
+    Parameters
+    ----------
+        micro_sec_per_frame : int
+            Frame timing
+        max_bytes_per_sec : int
+            Data rate
+        padding_granularity : int
+            Pad data to multiples of this value
+        flags : :py:class:`libavifile.enums.AVIF`
+            Flags
+        total_frames : int
+            Total number of frames
+        initial_frames : int
+            See _`AVIMAINHEADER`
+        streams : int
+            Number of streams
+        suggested_buffer_size : int
+            Suggested buffer size in bytes
+        width : int
+            Frame width in pixels
+        height : int
+            Frame height in pixels
+        reserved : list
+            A list of four integers
+
+    .. _AVIMAINHEADER: https://msdn.microsoft.com/en-us/library/windows/desktop/dd318180(v=vs.85).aspx
+
+    """
 
     def __init__(self, micro_sec_per_frame, max_bytes_per_sec,
                  padding_granularity, flags, total_frames,
@@ -35,6 +70,14 @@ class AviFileHeader(object):
 
 
 class AviFile(object):
+    """An AVI file
+
+    Parameters
+    ----------
+        file_or_filename
+            An open file (or file-like object) or a string containing a file name
+
+    """
 
     def __init__(self, file_or_filename):
         self.__file = file_or_filename
@@ -72,21 +115,33 @@ class AviFile(object):
 
     @property
     def avih(self):
+        """Convenience alias for self.avi_header"""
         return self.avi_header
 
     @property
     def strl(self):
+        """Convenience alias for self.stream_definitions"""
         return self.stream_definitions
 
     @property
     def movi(self):
+        """Convenience alias for self.stream_content"""
         return self.stream_content
 
     @property
     def idx1(self):
+        """Convenience alias for self.index_v1"""
         return self.index_v1
 
     def iter_frames(self, stream_id):
+        """Iterate over the frames in stream `stream_id`
+
+        Parameters
+        ----------
+            stream_id : int
+                Id of the stream to iterate over.
+
+        """
         stream_definition = self.stream_definitions[stream_id]
         decoder = DecoderBase.for_avi_stream(stream_definition=stream_definition)
         for stream_chunk in self.stream_content.iter_chunks(stream=stream_id):
@@ -94,5 +149,6 @@ class AviFile(object):
                                              keyframe=True)
 
     def close(self):
+        """Close the underlying file."""
         if not self.__file.closed:
             self.__file.close()
