48678614ceed6f83873b95cc4e43a9a46c06ece6
Michael Uhl <michael.uhl@gmail.com>
2018-03-31 14:02:17 -0400

    Add metadata.

---
+++ src/libavifile/__about__.py
@@ -0,0 +1,3 @@
+__author__ = "Michael Uhl"
+__license__ = "LGPL"
+__version__ = "0.9.0"
--- src/libavifile/__init__.py
+++ src/libavifile/__init__.py
@@ -1,6 +1,6 @@
+from libavifile.__about__ import *
 from libavifile.avi import AviFile
 from libavifile.decoder import DecoderBase
 import libavifile.rle
 import libavifile.bmp
 
-get_decoder_for_stream = DecoderBase.for_avi_stream
